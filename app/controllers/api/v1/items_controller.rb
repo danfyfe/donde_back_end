5be17f2ba11a381294851822cbe0c2fe8ba2718d
danfyfe <fyfedaniel@gmail.com>
2019-07-12 06:05:11 -0400

    twilio info removed

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -37,15 +37,15 @@ def update
   # byebug
   @message = Message.create(user_id: @user.id, household_id: @household.id, title:"#{@item.name} has been moved!", content:"#{@user.username} has moved #{@item.name}. #{@item.name} is now in #{@item.container.name} in #{@item.space.name}")
 
-  account_sid = 'AC05377eed0a2fe249abec193cbf88680a'
-  auth_token = 'd95fa324ce410cbcb3bfa0af5a38f4e8'
+  account_sid = ''
+  auth_token = ''
 
   @client = Twilio::REST::Client.new(account_sid, auth_token)
 
   message = @client.messages.create(
     body:"Message from Donde: #{@user.username} has moved #{@item.name}. #{@item.name} is now in #{@item.container.name} in #{@item.space.name}",
-    from:'+19733709632',
-    to:'+12016027876'
+    from:'+',
+    to:'+1'
   )
 
   render json: @item, include: [:users,:container,:space,:household]
