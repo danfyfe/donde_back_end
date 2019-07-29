0e0d80b6034d6c627054c8d741c3e697a385be36
danfyfe <fyfedaniel@gmail.com>
2019-07-29 10:10:47 -0400

    deploy3

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://df-donde.herokuapp.com'
 
     resource '*',
       headers: :any,
