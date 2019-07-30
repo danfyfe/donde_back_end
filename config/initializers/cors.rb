f579c4519dda4f590169f2a838b8c911a8d81ded
danfyfe <fyfedaniel@gmail.com>
2019-07-30 11:33:20 -0400

    cors change

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://df-donde.herokuapp.com'
+    origins 'https://df-donde.herokuapp.com', 'http://localhost:3001'
 
     resource '*',
       headers: :any,
